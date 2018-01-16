a8d0dbf3cf0952697a6467c43cf02569ea377bd8
Zhentian Wan <answer881215@gmail.com>
2018-01-16 12:30:21 +0200

    Update ramda-transducer.js

--- ramda-transducer.js
+++ ramda-transducer.js
@@ -1,58 +1,437 @@
 var t = require("transducers-js");
 var R = require("ramda");
 
+const timeIt = (label, fn) => {
+    console.time(label);
+    fn();
+    console.timeEnd(label);
+};
+
 const headerStr = `hostname, username `;
 const contentStr = `
     10.1.10.1, zwan
     10.1.11.1, wan
-
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
+    10.1.10.1, zwan
+    10.1.11.1, wan
 `;
-
-/**
- * Working with Ramda.js
- */
-const stringToArray = R.compose(
-    R.map(R.trim),
-    R.filter(Boolean),
-    R.split(',')
-);
-const transformContent = R.compose(
-    R.map(stringToArray),
-    R.filter(Boolean),
-    R.split('\n')
-);
 const keyValPair = R.useWith(
     R.map, [
         R.zipObj,
         R.identity
     ]
 );
+/**
+ * Working with Ramda.js
+ */
+const fn1 = () => {
+    const stringToArray = R.compose(
+        R.map(R.trim),
+        R.filter(Boolean),
+        R.split(',')
+    );
+    const transformContent = R.compose(
+        R.map(stringToArray),
+        R.filter(Boolean),
+        R.split('\n')
+    );
+    const header = stringToArray(headerStr);
+    const content = transformContent(contentStr);
 
-const header = stringToArray(headerStr);
-const content = transformContent(contentStr);
+    return keyValPair(header, content);
+}
 
-const res = keyValPair(header, content);
-console.log(res);
 
 /**
  * Working with transducer-js + Rmada.js
  */
+const fn2 = () => {
+    const stringToArr = R.compose(
+        t.comp(
+            t.map(R.trim),
+            t.filter(R.isNil)
+        ),
+        R.split(',')
+    );
+    const transformCnt = R.compose(
+        t.comp(
+            t.map(stringToArr),
+            t.filter(R.isNil)
+        ),
+        R.split('\n')
+    );
+    const h = stringToArr(headerStr);
+    const c = transformCnt(contentStr);
+    return keyValPair(h, c);
+}
 
-const stringToArr = R.compose(
-    t.comp(
-        t.map(R.trim),
-        t.filter(R.isNil)
-    ),
-    R.split(',')
-);
-const transformCnt = R.compose(
-    t.comp(
-        t.map(stringToArr),
-        t.filter(R.isNil)
-    ),
-    R.split('\n')
-);
-const h = stringToArr(headerStr);
-const c = transformCnt(contentStr);
-const res2 = keyValPair(h, c);
-console.log("res2", res);
+
+timeIt('transducer', fn2) //1.732ms
+timeIt('Ramda only', fn1) // 7.410ms
